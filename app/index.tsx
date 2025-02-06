479e8a0014b1e2e3878697c1149c10af18200ae0
Sr1515 <joao151576@gmail.com>
2025-02-06 19:03:25 -0300

    arquivos inicias do projeto

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import 'react-native-reanimated';
+
+
+export default function App() {
+
+  return (
+    <>
+      <Text style={{ textAlign: "center" }}>Bandido Classico</Text>
+    </>
+  );
+}
