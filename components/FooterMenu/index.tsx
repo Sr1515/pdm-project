4266ee2fd76a9a83ee4435d7dd0fdf16b8111438
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2025-02-08 11:11:54 -0300

    feat: implementa exemplo de uso de IonIcons

--- components/FooterMenu/index.tsx
+++ components/FooterMenu/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Footer, MenuItem, MenuText } from "./style";
 import { SvgUri } from 'react-native-svg';
+import Ionicons from "@expo/vector-icons/Ionicons"
 
 export const FooterMenu = () => {
     return (
         <Footer>
             <MenuItem>
-                <SvgUri
-                    width="24"
-                    height="24"
-                    uri="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/debian.svg"
-                />
+                <Ionicons name="home" size={32} color={"white"}/>
                 <MenuText>Home</MenuText>
             </MenuItem>
             <MenuItem>
